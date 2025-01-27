f313c8db12c114b5228fe2447d5890d325cea902
Iván García <igcarrasco@minsait.com>
2025-01-27 10:04:29 +0100

    first commit

---
+++ springboot-pedidos/src/main/java/com/ivan/frikitech/pedidos/springboot_pedidos/SpringbootPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.ivan.frikitech.pedidos.springboot_pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPedidosApplication.class, args);
+	}
+
+}
---
+++ springboot-pedidos/src/test/java/com/ivan/frikitech/pedidos/springboot_pedidos/SpringbootPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivan.frikitech.pedidos.springboot_pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
