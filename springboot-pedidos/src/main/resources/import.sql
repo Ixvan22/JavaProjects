25ec8af1ad93c534461e130bef3c10e3a0fbafcd
Iván García <igcarrasco@minsait.com>
2025-01-30 11:11:36 +0100

    feat: datos prueba

---
+++ springboot-pedidos/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO producto (nombre, descripcion, precio) VALUES ("Monitor", "Asus", 200);
+INSERT INTO producto (nombre, descripcion, precio) VALUES ("Portátil", "Dell", 1200);
+INSERT INTO producto (nombre, descripcion, precio) VALUES ("Ratón", "Logitech", 50);
+INSERT INTO pedido (producto_id, cantidad, total) VALUES (1, 2, 400);
+INSERT INTO pedido (producto_id, cantidad, total) VALUES (2, 1, 1200);
+INSERT INTO pedido (producto_id, cantidad, total) VALUES (3, 4, 200);
